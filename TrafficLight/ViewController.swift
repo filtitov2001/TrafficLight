a3f3e366fed50430520f12356ee24ea950881e6c
Felix Titov <titow.feliks@yandex.ru>
2021-08-18 23:18:02 +0300

    set signal logic

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var changeButton: UIButton!
+    
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redView.alpha = 0.3
+        yellowView.alpha = 0.3
+        greenView.alpha = 0.3
+        
+        redView.layer.cornerRadius = 75
+        yellowView.layer.cornerRadius = 75
+        greenView.layer.cornerRadius = 75
+        changeButton.layer.cornerRadius = 10
     }
 
-
+    @IBAction func changeButtonPressed() {
+        changeButton.setTitle("NEXT", for: .normal)
+        
+        if redView.alpha == yellowView.alpha {
+            redView.alpha = 1
+            greenView.alpha = 0.3
+        } else if redView.alpha == 1 && yellowView.alpha == greenView.alpha {
+            yellowView.alpha = 1
+            redView.alpha = 0.3
+        } else if yellowView.alpha == 1 && redView.alpha == greenView.alpha {
+            greenView.alpha = 1
+            yellowView.alpha = 0.3
+        }
+        
+    }
+    
 }
 
