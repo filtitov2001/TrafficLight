4fc709f7dfb50b2443667cb097e2583050b51269
Felix Titov <titow.feliks@yandex.ru>
2022-01-27 22:23:57 +0300

    Created stack view and changed logic

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,6 +7,10 @@
 
 import UIKit
 
+enum TrafficLightColors {
+    case red, yellow, green
+}
+
 class ViewController: UIViewController {
 
     @IBOutlet var changeButton: UIButton!
@@ -15,6 +19,8 @@ class ViewController: UIViewController {
     @IBOutlet var yellowView: UIView!
     @IBOutlet var greenView: UIView!
     
+    private var currentColor:TrafficLightColors = .red
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -22,24 +28,32 @@ class ViewController: UIViewController {
         yellowView.alpha = 0.3
         greenView.alpha = 0.3
         
-        redView.layer.cornerRadius = 75
-        yellowView.layer.cornerRadius = 75
-        greenView.layer.cornerRadius = 75
+        redView.layer.cornerRadius = redView.frame.width/2
+        yellowView.layer.cornerRadius = yellowView.frame.width/2
+        greenView.layer.cornerRadius = greenView.frame.width/2
         changeButton.layer.cornerRadius = 10
     }
 
     @IBAction func changeButtonPressed() {
-        changeButton.setTitle("NEXT", for: .normal)
-        
-        if redView.alpha == yellowView.alpha {
+        if changeButton.currentTitle == "START" {
+            changeButton.setTitle("NEXT", for: .normal)
             redView.alpha = 1
-            greenView.alpha = 0.3
-        } else if redView.alpha == 1 && yellowView.alpha == greenView.alpha {
-            yellowView.alpha = 1
+            return
+        }
+        
+        switch currentColor {
+        case .red:
             redView.alpha = 0.3
-        } else if yellowView.alpha == 1 && redView.alpha == greenView.alpha {
-            greenView.alpha = 1
+            yellowView.alpha = 1
+            currentColor = .yellow
+        case .yellow:
             yellowView.alpha = 0.3
+            greenView.alpha = 1
+            currentColor = .green
+        case .green:
+            greenView.alpha = 0.3
+            redView.alpha = 1
+            currentColor = .red
         }
         
     }
